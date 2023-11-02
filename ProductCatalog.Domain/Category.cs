d0e461a740648ffce6e601342e903c4fc003d82c
Danila Koltun <koltundanila@gmail.com>
2023-11-02 22:22:04 +0100

    Damain structure was added

---
+++ ProductCatalog.Domain/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductCatalog.Domain
+{
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+    }
+}
---
+++ ProductCatalog.Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductCatalog.Domain
+{
+    public class Product
+    {
+        public Guid ProductID { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid CategoryId { get; set; } 
+        public Category Category { get; set; }
+    }
+}
