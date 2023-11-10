301a59169a3d21d518576068ded57258be40cd88
Danila Koltun <koltundanila@gmail.com>
2023-11-10 17:46:16 +0100

    add ICategotyRepository

---
+++ ProductCatalog.Application/Common/Interfaces/ICategotyRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProductCatalog.Application.Application.Models.Category;
+
+namespace ProductCatalog.Application.Common.Interfaces
+{
+    public interface ICategotyRepository : IRepository
+    {
+        Task<Guid> CreateCategoryAsync(Guid categoryId);
+        Task UpdateCategoryAsync(UpdateCategoryCommand categoryCommand);
+        Task<CategoryDetailsDto> GetCategoryDetailsAsync(Guid categoryId);
+        Task<List<CategoryLookupDto>> GetCategoryLookupsAsync();
+        Task DeleteCategoryAsync(Guid categoryId);
+    }
+}
