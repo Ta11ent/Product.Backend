825c70725f7fe06147846222212dab1d5c91ed67
Danila Koltun <koltundanila@gmail.com>
2023-11-11 18:19:19 +0100

    add Fluent Validation and mapper pakages

--- ProductCatalog.Application/DependencyInjection.cs
+++ ProductCatalog.Application/DependencyInjection.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
 using ProductCatalog.Application.Common.Interfaces;
 using ProductCatalog.Application.Common.Mapping;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 namespace ProductCatalog.Application
@@ -12,12 +10,16 @@ namespace ProductCatalog.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddAutoMapper(
-                 config =>
+            services.AddMediatR(config => 
+            config.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly));
+
+            services.AddAutoMapper(config =>
                  {
                      config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
                      config.AddProfile(new AssemblyMappingProfile(typeof(IProductDbContext).Assembly));
                  });
+
+            services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
 
             return services;
         }
