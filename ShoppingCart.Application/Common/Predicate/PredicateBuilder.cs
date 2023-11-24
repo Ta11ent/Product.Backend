fae53839217647a1bcd1301abe3157d106ac3d3f
Danila Koltun <koltundanila@gmail.com>
2023-11-24 19:45:20 +0100

    Add Predicate Builder

--- ShoppingCart.Application/Common/Predicate/PredicateBuilder.cs
+++ ShoppingCart.Application/Common/Predicate/PredicateBuilder.cs
@@ -1,6 +1,27 @@
-﻿namespace ShoppingCart.Application.Common.Predicate
+﻿using System.Linq.Expressions;
+
+namespace ShoppingCart.Application.Common.Predicate
 {
-    public class PredicateBuilder
+    internal static class PredicateBuilder
     {
+        internal static Expression<Func<T, bool>> True<T>() { return response => true; }
+        internal static Expression<Func<T, bool>> And<T, P>(this Expression<Func<T, bool>> expr1,
+                                                             Expression<Func<T, bool>> expr2, P request)
+        {
+            if (string.IsNullOrWhiteSpace(request.ToString()))
+                expr2 = f => true;
+
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+                  (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+        internal static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
+                                                             Expression<Func<T, bool>> expr2)
+        {
+
+            var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>
+                  (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
     }
 }
