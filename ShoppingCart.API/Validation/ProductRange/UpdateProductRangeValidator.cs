cce01b9408b5827e7ca95eff6fb4b40cc88d1b30
Danila Koltun <koltundanila@gmail.com>
2023-11-26 20:44:43 +0100

    add validation

---
+++ ShoppingCart.API/Validation/ProductRange/UpdateProductRangeValidator.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingCart.API.Models.ProductRange;
+
+namespace ShoppingCart.API.Validation.ProductRange
+{
+    public class UpdateProductRangeValidator : AbstractValidator<UpdateProductRnageDto>
+    {
+        public UpdateProductRangeValidator() =>
+            RuleFor(x => x.Count).NotEmpty().GreaterThan(0).LessThan(100);
+    }
+}
---
+++ ShoppingCart.API/Validation/ValidationFilter.cs
@@ -0,0 +1,21 @@
+﻿namespace ShoppingCart.API.Validation
+{
+    public class ValidationFilter<T> : IEndpointFilter where T : class
+    {
+        private readonly IValidator<T> _validator;
+        public ValidationFilter(IValidator<T> validator) => _validator = validator;
+
+
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context,
+            EndpointFilterDelegate next)
+        {
+            var entity = context.Arguments
+                .FirstOrDefault(x => x.GetType() == typeof(T)) as T;
+            var result = await _validator.ValidateAsync(entity!);
+            if (!result.IsValid)
+                return Results.Json(result.Errors, statusCode: 400);
+
+            return await next(context);
+        }
+    }
+}
