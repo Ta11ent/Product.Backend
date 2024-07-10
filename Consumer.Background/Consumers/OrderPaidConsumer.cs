08907e7f043b9639b22898a7e2d088ebb75a80be
Danila Koltun <koltundanila@gmail.com>
2024-07-10 23:52:46 +0200

    fix MassTransitISsue

--- Consumer.Background/Consumers/OrderPaidConsumer.cs
+++ Consumer.Background/Consumers/OrderPaidConsumer.cs
@@ -5,7 +5,7 @@ namespace Consumer.Consumers
     public sealed class OrderPaidConsumer : IConsumer<OrderDetails>
     {
         private readonly ILogger<OrderPaidConsumer> _logger;
-        public OrderPaidConsumer(ILogger<OrderPaidConsumer> logger, IMessage  ) => _logger = logger;
+        public OrderPaidConsumer(ILogger<OrderPaidConsumer> logger) => _logger = logger;
         public async Task Consume(ConsumeContext<OrderDetails> context)
         {
             var jsonMessage = JsonConvert.SerializeObject(context.Message);
